a8688c998dedf4a34f2ceb097f43ab20391a4245
Aemantasneem <Aema0002@edu.easj.dk>
2021-03-04 16:31:17 +0100

    tema 2.1

--- includes/footer.php
+++ includes/footer.php
@@ -1,17 +1,38 @@
-<div class="footer">
-    <div>
-        <a href="#">link</a>
-        <a href="#">link</a>
-        <a href="#">link</a>
+
+<!-- i <body> har man alt indhold på siden som brugeren kan se -->
+<html lang="da">
+<body>
+
+<footer class="footer">
+    <div class="footer-app"<p class="">Hent vores app</p>
+    <a class="footer-app-link" href="https://itunes.apple.com/dk/app/dmi-vejr/id533069944">
+        <img alt="Nu i App store" src="Image/app store.png" height="50" width="100">
+    </a>
+    </a>
+    <a class="footer-app-link footer-app-link-google" href="https://play.google.com/store/apps/details?id=dk.dmi.byvejret">
+        <img alt="Nu på Google Play" src="Image/google play .png" height="50" width="100">
+    </a>
     </div>
-    <div>
-        <a href="#">link</a>
-        <a href="#">link</a>
-        <a href="#">link</a>
+    <div class="footer-sitemap">
+            <div class="Presse og medier">Presse og medier</div>
+            <div class="Job og karriere">Job og karriere</div>
+            <div class="Kontakt">Kontakt</div>
     </div>
-    <div>
-        <a href="#">link</a>
-        <a href="#">link</a>
-        <a href="#">link</a>
+
+    <div class="footer-mis">
+        <div id="footer-copyright" class="show-for-large large-3 columns">&copy; 2021 DMI All rights reserved</div>
+        <div id="footer-misc-menu" class="small-9 medium-9 large-6 columns">
+            <a href="https://www.twitter.com">
+                <img src="Image/Twitter.png" height="100" width="90"/>
+            </a>
+            <a href="https://www.facebook.com/">
+                <img src="Image/Facebook.png" height="95" width="100"
+            </a>
+        </div>
+
     </div>
-</div>
+
+</footer>
+</body>
+</html>
+
--- index.php
+++ index.php
@@ -9,7 +9,8 @@
     <!-- Sætter tegnsætning til utf-8 som bl.a. tillader danske bogstaver -->
     <meta charset="utf-8">
 
-    <!-- Titel som ses oppe i browserens tab mv. -->
+    <!-- Titel som ses oppe i browsere
+  ns tab mv. -->
     <title>Sigende titel</title>
 
     <!-- Metatags der fortæller at søgemaskiner er velkomne, hvem der udgiver siden og copyright information -->
@@ -28,9 +29,6 @@
 <body>
 <?php include"includes/navigation.php";?>
 
-<div class="main">
-    <div>forside</div>
-</div>
 
 <?php include"includes/footer.php"; ?>
 
