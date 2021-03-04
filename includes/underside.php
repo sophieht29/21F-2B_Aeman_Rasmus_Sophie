bada09b11ba61762f3d0f0761b9d017dfcb7d8b2
Aemantasneem <Aema0002@edu.easj.dk>
2021-03-04 17:07:15 +0100

    tema 2.1

--- includes/underside.php
+++ includes/underside.php
@@ -28,10 +28,11 @@
 <!-- i <body> har man alt indhold på siden som brugeren kan se -->
 <body>
 <?php include"includes/navigation.php";?>
-<?php include"includes/forside.php";?>
+
 <?php include"includes/nyheder.php";?>
 
 <?php include"includes/footer.php"; ?>
 
 </body>
 </html>
+
